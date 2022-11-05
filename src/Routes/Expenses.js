7f6a1c92dcae38598f113706ecb7d6f715d3301a
Shreyas Prabhu <csfxshreyas@gmail.com>
2022-11-05 13:06:06 +0530

    fetching expenses from backend

--- src/Routes/Expenses.js
+++ src/Routes/Expenses.js
@@ -1,7 +1,29 @@
+import { useEffect, useState } from "react";
+import API from "../utils/API";
+
 function Expenses() {
+  const [expenses, setExpenses] = useState([]);
+  useEffect(() => {
+    API.get("/expenses")
+      .then((res) => {
+        setExpenses(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
-    <div>Expenses</div>
-  )
+    <>
+      <h1>Expenses</h1>
+      {expenses.length &&
+        expenses.map((expense) => (
+          <div key={expense.id}>
+            <h2>{expense.id}</h2>
+          </div>
+        ))}
+    </>
+  );
 }
 
-export default Expenses
+export default Expenses;
